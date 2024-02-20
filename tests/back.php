9611248cec2d5b21e12065ede3debc731b14b0e4
Mihailmwm <mihailpankratov49735@gmail.com>
2024-02-20 13:44:33 +0300

    m+

---
+++ tests/back.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+ get_headers('https://mospolytech.ru/');
+?>
+<a href="index.htmal"> Переход на 1 экран</a>
+    
+</body>
+</html>
